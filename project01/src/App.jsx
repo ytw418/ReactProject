c0ef3553dd8ee14937a42c873ccaa7b990e0668e
ytw418 <ytw418@gmail.com>
2021-10-19 18:37:38 +0900

    JXS 파일로 확장자 변경

---
+++ project01/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Hello from './hello';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <header className="header">asdasd</header>
+    <div></div>
+    <div>dasd</div>
+    <Hello name="react"></Hello>
+    </>
+  );
+}
+
+export default App;
---
+++ project01/src/hello.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Hello(props) {
+    return (
+ 
+        <div>안녕하세여{props.name}</div>
+    );
+}
+
+export default Hello;
