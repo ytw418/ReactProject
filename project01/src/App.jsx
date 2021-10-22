dc947ca1862edc8d1778fbba9b7aa0d434196d02
ytw418 <ytw418@gmail.com>
2021-10-22 10:53:49 +0900

    props

--- project01/src/App.jsx
+++ project01/src/App.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import Hello from './hello';
 import './App.css';
+import Wrapper from './Wrapper';
 
 function App() {
   return (
-    <>
+    
+    <Wrapper>
     <header className="header">asdasd</header>
     <div></div>
     <div>dasd</div>
-    <Hello name="react"></Hello>
-    </>
+    <Hello name="react" color="red"></Hello>
+    </Wrapper>
+
   );
 }
 
--- project01/src/hello.jsx
+++ project01/src/hello.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
-function Hello(props) {
+function Hello({color,name}) {
     return (
- 
-        <div>안녕하세여{props.name}</div>
+        <div>
+        <div style={{ color : color }}>안녕하세여{name}</div>
+        </div>
     );
+
 }
+Hello.defaultProps = {
+    name: '이름없음'
+    }
 
 export default Hello;
