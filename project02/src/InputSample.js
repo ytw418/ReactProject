14e690ebe80cfaac596f1ad422d14f694825b26e
ytw418 <ytw418@gmail.com>
2021-10-29 16:13:06 +0900

    충돌제거

--- project02/src/InputSample.js
+++ project02/src/InputSample.js
@@ -15,13 +15,13 @@ function InputSample({안녕}) {
     return(
         <div>
             <input onChange={onChange} value={text}></input>
-<<<<<<< HEAD
+
             <button onClick={onReset}>{안녕}</button>
             <div>값 : {text}</div>
-=======
+
             <button onClick={onReset}>초기화</button>
             <div>값 :랜더링 테스트 {text}</div>
->>>>>>> 6acd8f5e7239d0bcc2c7c931c9a445b1d458a8be
+
 
         </div>
     );
