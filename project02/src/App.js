eba76167a1e77e2803927bee7c8894a6a3bb1054
ytw418 <ytw418@gmail.com>
2022-03-16 17:37:43 +0900

    useMemo 는 특정( 결과값)을 재사용 할 때 사용하는 반면, useCallback 은 특정 (함수)를 새로 만들지 않고 재사용하고 싶을때 사용합니다.

--- project02/src/App.js
+++ project02/src/App.js
@@ -25,14 +25,15 @@ function App() {
   const {username,email} = inputs;
   const nextId = useRef(4); // useRef 로 선언하면 컴포넌트가 리랜더링 되더라도 변수값이 초기화 되지 않는다 = 전역저장소에 저장되기 때문
 
-  const onChange = e => {
+  const onChange = useCallback( e => {
     const {name, value} = e.target;
     setInputs({
       ...inputs,
       [name]:value  // 네임=키 (네임은 username, 또는 email 이다 )에 값은 벨류다
     })
   
-  }
+  },[inputs]
+  );
 
   const [users,setUsers] = useState([
     
@@ -62,7 +63,7 @@ function App() {
   
 
   const onCreate = useCallback(() => {
-
+    
     const user = {
       id: nextId.current,
       username,
@@ -81,11 +82,12 @@ function App() {
     nextId.current += 1;
   },[users,username,email]);
 
-  const onRemove = (iddd)  => {
+  const onRemove = useCallback((iddd)  => {
     // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
     // = user.id 가 id 인 것을 제거함
     setUsers(users.filter(user => user.id !== iddd)); // 같은 아이디가 아닌 것 만 추출함(조건 같은 개념이네) 
-  };
+  }
+  ,[users]);
 
 
 //user.active 변수를 수정하고 리턴 user 코드
@@ -101,13 +103,15 @@ function App() {
 
 // ...user 로 불변성 유지코드
 
-  const onToggle = id => {
+  const onToggle = useCallback(
+    id => {
+    
     setUsers(
       users.map(user =>
         user.id === id ? { ...user, active: !user.active } : user
       )
     );
-  };
+  },[users]);
 
   const count = useMemo( () => {
     console.log('활성 사용자 수를 세는중...');
--- project02/src/UserList.js
+++ project02/src/UserList.js
@@ -1,33 +1,38 @@
-import React, { useEffect } from 'react'
+import React from 'react';
 
-function User({ user,onRemove,onToggle }) {
-    useEffect(() => {
-        console.log('user 값이 설정됨');
-        console.log(user);
-        return () => {
-          console.log('user 가 바뀌기 전..');
-          console.log(user);
-        };
-      }, [user]);
-    return (
-        <div>
-            <b style={{cursor:"pointer",color:user.active ? 'green':'black'}}onClick={()=> onToggle(user.id)}>{user.username}</b> <span>({user.email})</span>
-            <button onClick={() => onRemove(user.id)}>삭제</button>
-        </div>
-    );
+const User = React.memo(function User({ user, onRemove, onToggle }) {
+  return (
+    <div>
+      <b
+        style={{
+          cursor: 'pointer',
+          color: user.active ? 'green' : 'black'
+        }}
+        onClick={() => onToggle(user.id)}
+      >
+        {user.username}
+      </b>
+      &nbsp;
+      <span>({user.email})</span>
+      <button onClick={() => onRemove(user.id)}>삭제</button>
+    </div>
+  );
+});
+
+function UserList({ users, onRemove, onToggle }) {
+    console.log('asd');
+  return (
+    <div>
+      {users.map(user => (
+        <User
+          user={user}
+          key={user.id}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
+      ))}
+    </div>
+  );
 }
 
-// 컴포넌트 두개 생성가능
-
-export default function UserList({users,onRemove, onToggle}) {
-
-
-    return (
-        <div>
-            {users.map(u => (
-                <User user={u} key={u.id} onRemove={onRemove}  onToggle={onToggle}  />
-            ))}
-        </div>
-    )
-}
-
+export default React.memo(UserList);
